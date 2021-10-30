a7fa5cd3cfb7bf1476a059e7642c16a940d3f33b
afeezdev <afeezdev@gmail.com>
2021-10-30 01:40:49 +0100

    Routing

---
+++ pages/product/[productId]/index.js
@@ -0,0 +1,9 @@
+import { useRouter } from 'next/router'
+
+ function Review() {
+   const router = useRouter()
+   const productId = router.query.productId
+    return <h1>Product Detail  {productId}</h1>
+  }
+  
+  export default Review
