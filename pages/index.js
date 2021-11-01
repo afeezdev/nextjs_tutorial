52813f86cc8b0ab47bfbde813cde5d8539dc53e9
afeezdev <afeezdev@gmail.com>
2021-11-01 13:11:29 +0100

    getStaticProps and getStaticPath

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,15 @@
-
+import Link from 'next/link'
 
 function Home() {
     return (
   <div>
     <h1>Next JS pre-rendering</h1>
+    <Link href="/users">
+    <a>Users</a>
+    </Link>
+    <Link href="/posts">
+    <a>Posts</a>
+    </Link>
   </div>
   )
 }
