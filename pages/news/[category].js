0c7ded6a6e48154c3fb102de138356d490570424
afeezdev <afeezdev@gmail.com>
2021-11-05 17:11:09 +0100

    context parameter in getServerSideProps

---
+++ pages/news/[category].js
@@ -0,0 +1,46 @@
+import { useRouter } from "next/router"
+
+function ArticleListByCategory({ articles, category }) {
+    return (
+            <div>
+                <h2>Showing news for category <i>{category}</i>
+                </h2>
+                {
+                    articles.map(article =>{
+                        return(
+                            <div key={article.id}>
+                                <h2>
+                                    {article.id} {article.title}
+                                </h2>
+                                <p>{article.description}</p>
+                                <hr/>
+                            </div>
+                        )
+                    })
+                }
+            </div>
+    )
+    
+}
+
+export default ArticleListByCategory
+
+export async function getServerSideProps(context) {
+    const { params, req, res, query } = context
+    console.log(query)
+    console.log(req.headers.cookie)
+    res.setHeader('Set-Cookie', ['name=afeezdev'])
+    const { category } = params
+    const response = await fetch(
+        `http://localhost:4000/news?category=${category}`)
+    
+        const data = await response.json()
+     
+    return {
+        props: {
+            articles: data,
+            category,
+        },
+    }
+    
+}
